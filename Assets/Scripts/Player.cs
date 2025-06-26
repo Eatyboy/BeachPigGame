59f86e9bf48826b37c52e56d4db42c6b25b6bc25
Eatyboy <141588549+Eatyboy@users.noreply.github.com>
2025-06-26 12:40:17 -0700

    Finished initial movement and camera system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,11 +10,15 @@ public class Player : MonoBehaviour
 
     [SerializeField] private Rigidbody rb;
     [SerializeField] private CapsuleCollider col;
+    [SerializeField] private CinemachineOrbitalFollow cinemachineOrbitalFollow;
 
     [Header("Parameters")]
 
-    [Tooltip("The number of units per second by which the player can move")] 
+    [Tooltip("The speed which the player can move in units per second")] 
     [SerializeField, Min(0.0f)] private float movementSpeed;
+
+    [Tooltip("The rate at which the player turns to match the camera in degrees per second")]
+    [SerializeField, Min(0.0f)] private float turnRate;
 
     [Tooltip("The number of units that the player can jump")] 
     [SerializeField, Min(0.0f)] private float jumpHeight;
@@ -29,6 +34,7 @@ public class Player : MonoBehaviour
     public Vector3 velocity;
 
     [SerializeField] private float GROUND_CHECK_DISTANCE = 0.1f;
+    [SerializeField] private float AUTO_ROT_THRESHOLD = 10f;
 
     [ContextMenu("Recompute Constants")]
     private void InitDerivedConsts()
@@ -75,9 +81,19 @@ public class Player : MonoBehaviour
             rb.linearVelocity = new(rb.linearVelocity.x, newYVel, rb.linearVelocity.z);
         }
 
-        Vector3 playerRight = Vector3.Cross(Vector3.up, transform.forward);
-        Vector3 moveDir = transform.forward * moveInput.y + playerRight * moveInput.x;
-        Vector3 moveVel = movementSpeed * moveDir;
+        float camYaw = cinemachineOrbitalFollow.HorizontalAxis.Value;
+        Vector3 camFwd = new(Mathf.Sin(camYaw * Mathf.Deg2Rad), 0.0f, Mathf.Cos(camYaw * Mathf.Deg2Rad));
+        Vector3 camRight = Vector3.Cross(Vector3.up, camFwd);
+        Vector3 targetMoveDir = camFwd * moveInput.y + camRight * moveInput.x;
+        float moveMagnitude = Mathf.Clamp01(moveInput.magnitude);
+
+        if (moveMagnitude != 0.0f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(targetMoveDir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, turnRate * fdt);
+        }
+
+        Vector3 moveVel = moveMagnitude * movementSpeed * transform.forward;
 
         rb.linearVelocity = new(moveVel.x, rb.linearVelocity.y, moveVel.z);
     }
@@ -92,8 +108,7 @@ public class Player : MonoBehaviour
     private void CheckGrounded()
     {
         Vector3 origin = transform.TransformPoint(col.center) 
-            + (col.height / 2.0f - 0.01f) * Vector3.down;
+            + (col.radius - 0.01f) * Vector3.down;
         onGround = Physics.Raycast(origin, Vector3.down, out RaycastHit hit, GROUND_CHECK_DISTANCE);
-        Debug.DrawRay(origin, Vector3.down * GROUND_CHECK_DISTANCE, onGround ? Color.green : Color.red);
     }
 } 
